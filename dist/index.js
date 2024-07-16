e45004060912643e730b365e0f99a22429f8e0a6
moaztobok <moaztobok@gmail.com>
2024-07-16 13:06:45 +0100

    build testing

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+const app_1 = require("./config/app");
+(0, app_1.run)();
+const PORT = process.env.PORT;
+const gallery_1 = __importDefault(require("./routes/gallery"));
+app_1.app.use('/', (req, res) => {
+    res.send('Hello World');
+});
+app_1.app.use('/api/gallery', gallery_1.default);
+app_1.app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`);
 });
