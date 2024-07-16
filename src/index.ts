3eb2c90c263a7aec689f6233c4fdac7898bacad1
moaztobok <moaztobok@gmail.com>
2024-07-16 12:43:27 +0100

    deployment testing

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ run();
 const PORT = process.env.PORT;
 
 import galleryRouter from './routes/gallery';
-
+app.use('/', (req, res) => {
+    res.send('Hello World')
+})
 app.use('/api/gallery', galleryRouter)
 
 app.listen(PORT, () => {
